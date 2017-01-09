6a69f34dec6b35ba31d739cd6abdbce421ebabc8
Ross-Gosling <rossjgosling@gmail.com>
2017-01-09 05:37:05 +0000

    Added placeholder update() and render() methods

--- Projects/GameEngine/src/player.cpp
+++ Projects/GameEngine/src/player.cpp
@@ -10,3 +10,15 @@ Player::Player()
 {
 	// TODO
 }
+
+// Void: Called to update the player model
+void Player::update(float fElapsedTime)
+{
+	// TODO
+}
+
+// Void: Called to render the player model
+void Player::render()
+{
+	// TODO
+}
