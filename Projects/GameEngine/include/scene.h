4cb7a6d7dd3542ae7c77c0e4aec3c7fc33f4198c
Ross-Gosling <rossjgosling@gmail.com>
2017-01-10 08:40:40 +0000

    Added Player and Model members to the Scene class

--- Projects/GameEngine/include/scene.h
+++ Projects/GameEngine/include/scene.h
@@ -2,7 +2,10 @@
 #define SCENE_H
 
 // Imports
-// TODO
+#include "player.h"
+#include "model.h"
+#include <memory>
+#include <vector>
 
 /////////////////////////////////////////////////
 ///
@@ -12,6 +15,8 @@
 class Scene
 {
 private:
+	Player m_player = Player(0.0f, 0.0f, 0.0f, 0.0f); //!< Player member for character handling
+	std::vector<std::shared_ptr<Model>> m_models; //!< Vector of Model pointers to associated Models in the Scene
 
 protected:
 
