01adde961ae41025a7959b15e517f3e673901291
Ross-Gosling <rossjgosling@gmail.com>
2017-01-10 06:41:13 +0000

    Added placeholder loadFromObj() and draw() methods

--- Projects/GameEngine/src/model.cpp
+++ Projects/GameEngine/src/model.cpp
@@ -10,3 +10,15 @@ Model::Model()
 {
 	// TODO
 }
+
+// Void: Loads an obj file into the class
+void Model::loadFromObj(const std::string ksFilePath)
+{
+	// TODO
+}
+
+// Void: Draws the Model
+void Model::draw()
+{
+	// TODO
+}
