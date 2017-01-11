44d981e117dd5e7dd7eddb8176e7fced67a22da0
Ross-Gosling <rossjgosling@gmail.com>
2017-01-11 18:27:23 +0000

    Added keyboard processing from prior work with relavent updates

--- Projects/GameEngine/include/player.h
+++ Projects/GameEngine/include/player.h
@@ -44,6 +44,15 @@ public:
 
 	/////////////////////////////////////////////////
 	///
+	/// \brief Processes keyboard input
+	///
+	/// \param kiKey The pressed key
+	///
+	///////////////////////////////////////////////// 
+	void processKeyInput(const int kiKey);
+
+	/////////////////////////////////////////////////
+	///
 	/// \brief Moves the Player forward by a given distance
 	///
 	/// \param kfDist The distance to move the Player
