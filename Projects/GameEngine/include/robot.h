1154fade5470a1254da2962d2054e4ae2085a59c
Ross-Gosling <rossjgosling@gmail.com>
2017-01-11 19:38:34 +0000

    Updated Robot with enums instead of const chars and added Doxygen compatible comments to Robot.h

--- Projects/GameEngine/include/robot.h
+++ Projects/GameEngine/include/robot.h
@@ -3,47 +3,110 @@
 
 #include "glm.hpp"
 
-// constants for arm and leg movement states
-const char BACKWARD_STATE = 0;
-const char FORWARD_STATE = 1;
-
-// index constants for accessing arm and leg array data
-const char LEFT = 0;
-const char RIGHT = 1;
-
 class Robot
 {
 private:
 
-	char m_cLegStates[2];
-	char m_cArmStates[2];
+	enum side { LEFT, RIGHT }; //!< Side enum for handling robot limbs
+	enum moveState { FORWARD, BACKWARD }; //!< Movement state for limb control
 
-	float m_fLegAngles[2];
-	float m_fArmAngles[2];
-
-	// draws a unit cube
+	moveState m_legStates[2]; //!< MoveState array for left&right leg movement handling
+	moveState m_armStates[2]; //!< MoveState array for left&right arm movement handling
+	
+	float m_fLegAngles[2]; //!< Float array for left&right leg movement handling
+	float m_fArmAngles[2]; //!< Float array for left&right arm movement handling
+	
+	/////////////////////////////////////////////////
+	///
+	/// \brief Draws a 1x1x1 cube at a specified position
+	///
+	/// \param kPosition Vector for cube position
+	///
+	///////////////////////////////////////////////// 
 	void drawCube(const glm::vec3 kPosition);
 
-	// methods to draw the parts of the robot
+	/////////////////////////////////////////////////
+	///
+	/// \brief Draws the Robot's arm at a specified position
+	///
+	/// \param kPosition Vector for arm position
+	///
+	///////////////////////////////////////////////// 
 	void drawArm(const glm::vec3 kPosition);
+
+	/////////////////////////////////////////////////
+	///
+	/// \brief Draws the Robot's head at a specified position
+	///
+	/// \param kPosition Vector for head position
+	///
+	///////////////////////////////////////////////// 
 	void drawHead(const glm::vec3 kPosition);
+
+	/////////////////////////////////////////////////
+	///
+	/// \brief Draws the Robot's torso at a specified position
+	///
+	/// \param kPosition Vector for torso position
+	///
+	///////////////////////////////////////////////// 
 	void drawTorso(const glm::vec3 kPosition);
+
+	/////////////////////////////////////////////////
+	///
+	/// \brief Draws the Robot's leg at a specified position
+	///
+	/// \param kPosition Vector for leg position
+	///
+	///////////////////////////////////////////////// 
 	void drawLeg(const glm::vec3 kPosition);
+
+	/////////////////////////////////////////////////
+	///
+	/// \brief Draws the Robot's foot at a specified position
+	///
+	/// \param kPosition Vector for foot position
+	///
+	///////////////////////////////////////////////// 
 	void drawFoot(const glm::vec3 kPosition);
 
 public:
 
+	bool m_bArmsMoving; //!< Boolean for arm movement control
+	bool m_bLegsMoving; //!< Boolean for leg movement control
+
+	/////////////////////////////////////////////////
+	///
+	/// \brief Constructor
+	///
+	///////////////////////////////////////////////// 
 	Robot();
+
+	/////////////////////////////////////////////////
+	///
+	/// \brief Denstructor
+	///
+	///////////////////////////////////////////////// 
 	~Robot() {};
 
-	bool m_bArmsMoving;
-	bool m_bLegsMoving;
-
-	// draws the entire robot
-	void draw(const glm::vec3 kPosition, const float kfRotation);
-
-	// updates the robot data
+	/////////////////////////////////////////////////
+	///
+	/// \brief Called to update the Robot model
+	///
+	/// \param kfElapsedTime The time since last update
+	///
+	///////////////////////////////////////////////// 
 	void update(const float kfElapsedTime);
+
+	/////////////////////////////////////////////////
+	///
+	/// \brief Called to render the Robot model
+	///
+	/// \param kPosition Vector for Robot position
+	/// \param kfRotation Float for Robot rotation angle
+	///
+	///////////////////////////////////////////////// 
+	void draw(const glm::vec3 kPosition, const float kfRotation);
 };
 
 #endif
