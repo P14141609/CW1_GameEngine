c51ff0c572bb474e7eca976d783e1db7d6d4b7b6
Ross-Gosling <rossjgosling@gmail.com>
2017-01-11 18:27:23 +0000

    Added keyboard processing from prior work with relavent updates

--- Projects/GameEngine/src/player.cpp
+++ Projects/GameEngine/src/player.cpp
@@ -12,6 +12,48 @@ Player::Player(const float kfXPos, const float kfYPos, const float kfZPos, const
 	m_rotation = kfRotation;
 }
 
+// Void: Processes keyboard input
+void Player::processKeyInput(const int kiKey)
+{
+	switch (kiKey)
+	{
+		// If input is GLFW_KEY_SPACE
+		case GLFW_KEY_SPACE:
+		{
+			// Inverts the current bool values for limb movement
+			m_pRobot->m_armsMoving = !m_pRobot->m_armsMoving;
+			m_pRobot->m_legsMoving = !m_pRobot->m_legsMoving;
+		} break;
+
+		// If input is GLFW_KEY_UP
+		case GLFW_KEY_UP:
+		{
+			moveForward(0.5f);
+		} break;
+
+		// If input is GLFW_KEY_DOWN
+		case GLFW_KEY_DOWN:
+		{
+			moveForward(-0.5f);
+		} break;
+
+		// If input is GLFW_KEY_LEFT
+		case GLFW_KEY_LEFT:
+		{
+			m_rotation += 2.5f;
+		} break;
+
+		// If input is GLFW_KEY_RIGHT
+		case GLFW_KEY_RIGHT:
+		{
+			m_rotation += -2.5f;
+		} break;
+
+		// Default case
+		default: break; // Do Nothing
+	}
+}
+
 // Void: Moves the Player forward
 void Player::moveForward(const float kfDist)
 {
